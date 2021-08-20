c7ab00268141b00775b997df93920ae413edbadf
redxzeta <xzetanzero@gmail.com>
2021-08-19 18:12:44 -0700

    bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Container } from "react-bootstrap";
+import QuizCard from "./components/QuizCard";
+
+function App() {
+  return (
+    <main>
+      <Container>
+        <h1>TEST</h1>
+        <QuizCard />
+      </Container>
+    </main>
+  );
+}
+
+export default App;
