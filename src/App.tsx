9ad1a15102900606bd3219cbaf44c814ffce41fb
redxzeta <xzetanzero@gmail.com>
2021-08-19 18:52:27 -0700

    fetcing data from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import QuizCard from "./components/QuizCard";
 
 function App() {
+  const [quiz, setQuiz] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${process.env.REACT_APP_EXPRESS}`);
+      const data = await response.json();
+      setQuiz(data);
+    };
+    fetchData();
+  }, []);
+  console.log(quiz);
   return (
     <main>
       <Container>
